41b284d3cc25db16da0964cc22e98089b91704b9
Embla Øye <embla.oye@bouvet.no>
2024-09-04 15:16:58 +0200

    Refactoring and move comments to readme

--- www/README.md
+++ www/README.md
@@ -2,6 +2,27 @@
 1) Run the dexpi.rdfox script, the documentation for this script is found [here](../rdfox/README.md)
 2) Open the dexpi.html file in a browser
 3) Select the boundary points and an internal component
-- Right click to select or deselect the internal component
-- Left click to select or deselect boundary nodes 
+   - Right click to select or deselect the internal component
+     1) If the node is not a member of the internal class:
+        - Add the node to the internal class to indicate that it is selected 
+        - Update rdfox with the following triple :nodeId a :insideBoundary .
+     2) If the node is a member of the internal class:
+        - Remove the node from the internal class to indicate that it is deselected 
+        - Update rdfox, delete the following triple :nodeId a :insideBoundary .
+     3) If the node is not a member of the internal class, but is a member of the boundary class:
+        - Add the node to the internal class, and remove it from the boundary class. 
+        - Update rdfox, insert the following triple :nodeId a :insideBoundary . 
+        - Update rdfox, delete the following triple :nodeId a :boundary .
+
+   - Ctrl+Right click to select or deselect boundary nodes
+     1) If the node is not a member of the boundary class:
+        - Add the node to the boundary class to indicate that it is selected 
+        - Update rdfox with the following triple :nodeId a :boundary .
+     2) If the node is a member of the boundary class:
+        - Remove the node from the boundary class to indicate that it is deselected 
+        - Update rdfox, delete the following triple :nodeId a :boundary .
+     3) If the node is not a member of the boundary class, but is a member of the internal class:
+        - Add the node to the boundary class, and remove it from the internal class. 
+        - Update rdfox, insert the following triple :nodeId a :boundary . 
+        - Update rdfox, delete the following triple :nodeId a :insideBoundary .
 4) Hit enter to query RDFox for the boundary - the output of the query can be found in the logs.
--- www/script.js
+++ www/script.js
@@ -7,67 +7,55 @@ document.addEventListener('keyup', async (event) => {
     }
 });
 
-/**
- * Left clicking is used to select or deselect nodes as boundries:
- * 1) If the node is not a member of the boundary class:
- *      - Add the node to the boundary class to indicate that it is selected
- *      - Update rdfox with the following triple :nodeId a :boundary .
- * 2) If the node is a member of the boundary class:
- *      - Remove the node from the boundary class to indicate that it is deselected
- *      - Update rdfox, delete the following triple :nodeId a :boundary .
- * 3) If the node is not a member of the boundary class, but is a member of the internal class:
- *      - Add the node to the boundary class, and remove it from the internal class.
- *  *   - Update rdfox, insert the following triple :nodeId a :boundary .
- *      - Update rdfox, delete the following triple :nodeId a :insideBoundary .
- */
-nodes.forEach( function(node) {
-    node.addEventListener('click', async () => {
+nodes.forEach((node) => {
+    node.addEventListener('click', async (event) => {
+        await handleNodeClick(node, event);
+    });
+});
+
+window.addEventListener('unload', async () => {
+    await handleWindowUnload();
+});
+
+async function handleNodeClick(node, event) {
+    let action, type;
+    if (event.ctrlKey) {
+        type = 'insideBoundary';
+        if (node.classList.contains('internal')) {
+            node.classList.remove('internal');
+            action = 'delete';
+        } else {
+            node.classList.add('internal');
+            action = 'insert';
+            if (node.classList.contains('boundary')) {
+                node.classList.remove('boundary');
+                await makeSparqlAndUpdateStore(node.id, 'delete', 'boundary');
+            }
+        }
+    } else {
+        type = 'boundary';
         if (node.classList.contains('boundary')) {
             node.classList.remove('boundary');
-            await makeSparqlAndUpdateStore(node.id, 'delete', 'boundary');
+            action = 'delete';
         } else {
             node.classList.add('boundary');
-            await makeSparqlAndUpdateStore(node.id, 'insert', 'boundary');
+            action = 'insert';
             if (node.classList.contains('internal')) {
                 node.classList.remove('internal');
                 await makeSparqlAndUpdateStore(node.id, 'delete', 'insideBoundary');
             }
         }
-    });
+    }
+    await makeSparqlAndUpdateStore(node.id, action, type);
+}
 
-/**
- * Right clicking is used to select or deselect a node as internal:
- * 1) If the node is not a member of the internal class:
- *      - Add the node to the internal class to indicate that it is selected
- *      - Update rdfox with the following triple :nodeId a :insideBoundary .
- * 2) If the node is a member of the internal class:
- *      - Remove the node from the internal class to indicate that it is deselected
- *      - Update rdfox, delete the following triple :nodeId a :insideBoundary .
- * 3) If the node is not a member of the internal class, but is a member of the boundary class:
- *      - Add the node to the internal class, and remove it from the boundary class.
- *  *   - Update rdfox, insert the following triple :nodeId a :insideBoundary .
- *      - Update rdfox, delete the following triple :nodeId a :boundary .
- */
-    node.addEventListener('contextmenu', async () => {
-        if (node.classList.contains('internal')) {
-            node.classList.remove('internal');
-            await makeSparqlAndUpdateStore(node.id, 'delete', 'insideBoundary');
-        } else {
-            node.classList.add('internal');
-            await makeSparqlAndUpdateStore(node.id, 'insert', 'insideBoundary');
-            if(node.classList.contains('boundary')){
-                node.classList.remove('boundary');
-                await makeSparqlAndUpdateStore(node.id, 'delete', 'boundary');
-            }
-        }
-    });
-
-    window.addEventListener('unload', async () => {
+async function handleWindowUnload(nodes) {
+    for (let node of nodes) {
         node.classList.remove('internal', 'boundary', 'inCommissioningPackage');
         let type = node.classList.contains('internal') ? 'insideBoundary' : 'boundary';
         await makeSparqlAndUpdateStore(node.id, 'delete', type);
-    });
-});
+    }
+}
 
 async function makeSparqlAndUpdateStore(nodeId, action, type) {
     let sparql = action === 'insert'
