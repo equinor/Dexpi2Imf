7722eab2666381d938247acf1e9b82c4163f0538
Nora Kristiansen <106915799+NoraKri@users.noreply.github.com>
2025-01-22 09:39:47 +0100

    add node type to frontend

--- www/src/types/CommissioningPackage.ts
+++ www/src/types/CommissioningPackage.ts
@@ -4,7 +4,11 @@ export default interface CommissioningPackage {
   id: string;
   name: string;
   color: HighlightColors;
-  boundaryIds: string[];
-  internalIds: string[];
-  selectedInternalIds: string[];
+  boundaryIds: Node[];
+  internalIds: Node[];
+  selectedInternalIds: Node[];
+}
+
+export interface Node {
+  id: string;
 }
