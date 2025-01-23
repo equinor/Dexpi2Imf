d2bc9ec94d34d9358015caea9f97a4afe7e5037f
henriette.rebnor <henriette.rebnor@bouvet.no>
2025-01-23 16:24:47 +0100

    query utils

---
+++ client/Boundaries/Backend/Utils/QueryUtils.cs
@@ -0,0 +1,25 @@
+﻿using static Boundaries.RdfoxApi;
+
+namespace Backend.Utils;
+
+public static class QueryUtils
+{
+    public static async Task<bool> IsBoundaryOf(string packageId, string nodeId, ConnectionSettings conn) 
+        =>  await AskSparql(conn, $@"ASK WHERE {{ <{nodeId}> {PropertiesProvider.isSelectedInternalOf} <{packageId}> }}");
+
+    public static async Task<bool> IsSelectedInternalOf(string packageId, string nodeId, ConnectionSettings conn)
+        => await AskSparql(conn, $@"ASK WHERE {{ <{nodeId}> {PropertiesProvider.isSelectedInternalOf} <{packageId}> }}");
+
+    public static async Task DeleteSelectedInternalOf(string packageId, string nodeId, ConnectionSettings conn)
+        => await DeleteData(conn, $@"<{nodeId}> {PropertiesProvider.isSelectedInternalOf} <{packageId}> .");
+
+    public static async Task DeleteBoundaryOf(string packageId, string nodeId, ConnectionSettings conn)
+        => await DeleteData(conn, $@"<{nodeId}> {PropertiesProvider.isBoundaryOf} <{packageId}> .");
+
+    public static async Task AddBounaryOf(string packageId, string nodeId, ConnectionSettings conn)
+    => await LoadData(conn, $@"<{nodeId}> {PropertiesProvider.isBoundaryOf} <{packageId}> .");
+
+    public static async Task AddSelectedInternalOf(string packageId, string nodeId, ConnectionSettings conn)
+        => await LoadData(conn, $@"<{nodeId}> {PropertiesProvider.isSelectedInternalOf} <{packageId}> .");
+
+}
