86fe1ffa43629ecee878cc698f5a12d80d07a033
henriette.rebnor <henriette.rebnor@bouvet.no>
2025-01-23 16:25:39 +0100

    move these in the future, not now to avvoid merge conflict

--- client/Boundaries/Backend/Utils/PropertiesProvider.cs
+++ client/Boundaries/Backend/Utils/PropertiesProvider.cs
@@ -3,10 +3,11 @@
     public static class PropertiesProvider
     {
         public static readonly string isBoundaryOf = "comp:isBoundaryOf" ;
+        public static readonly string isSelectedInternalOf = "comp:isSelectedInternalOf";
         public static readonly string isInPackage = "comp:isInPackage";
         public static readonly string adjacentTo = "imf:adjacentTo" ;
-        public static readonly string CommissioningPackage = "comp:CommissioningPackage" ;
-        public static readonly string CompletionPackage = "comp:CompletionPackage";
+        public static readonly string CommissioningPackage = "comp:CommissioningPackage" ; //TODO move to type provider
+        public static readonly string CompletionPackage = "comp:CompletionPackage";        //TODO move to type provider
         public static readonly string hasName = "comp:hasName";
         public static readonly string hasColor = "comp:hasColour";
     }   
