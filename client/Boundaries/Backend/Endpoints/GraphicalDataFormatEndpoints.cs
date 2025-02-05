84eff090c659f14bc1766897baff819425228cfd
Embla Øye <embla.oye@bouvet.no>
2025-02-05 10:33:23 +0100

    fix file path

--- client/Boundaries/Backend/Endpoints/GraphicalDataFormatEndpoints.cs
+++ client/Boundaries/Backend/Endpoints/GraphicalDataFormatEndpoints.cs
@@ -4,9 +4,9 @@ public static class GraphicalDataFormatEndpoints
 {
     public static void MapGraphicalDataFormatEndpoints(this IEndpointRouteBuilder endpoints)
     {
-        endpoints.MapGet("graphical-data-format", async () =>
+        endpoints.MapGet("/graphical-data-format", async () =>
         {
-            var filePath = Path.Combine(AppContext.BaseDirectory, "Backend", "Example", "graphical-data-format-example.json");
+            var filePath = Path.Combine(AppContext.BaseDirectory, "Example", "graphical-data-format-example.json");
             var jsonContent = await File.ReadAllTextAsync(filePath);
             return Results.Json(jsonContent);
         }).WithTags("Graphical Data Format");
