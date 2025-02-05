81e716b90a715c364932874a2c2fdf205a933007
Embla Øye <embla.oye@bouvet.no>
2025-02-05 08:18:24 +0100

    add graphical data format endpoint and refactor

---
+++ client/Boundaries/Backend/Endpoints/GraphicalDataFormatEndpoints.cs
@@ -0,0 +1,16 @@
+﻿using Boundaries;
+
+namespace Backend.Endpoints;
+
+public static class GraphicalDataFormatEndpoints
+{
+    public static void MapGraphicalDataFormatEndpoints(this IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapGet("graphical-data-format", async () =>
+        {
+            var filePath = Path.Combine(AppContext.BaseDirectory, "Backend", "Example", "graphical-data-format-example.json");
+            var jsonContent = await File.ReadAllTextAsync(filePath);
+            return Results.Json(jsonContent);
+        }).WithTags("Graphical Data Format");
+    }
+}
