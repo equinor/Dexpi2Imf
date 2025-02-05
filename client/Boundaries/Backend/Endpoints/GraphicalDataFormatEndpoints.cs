ddd22102dc026a4eddf1bdfd155adf5ee2ce37fc
Embla Øye <embla.oye@bouvet.no>
2025-02-05 11:03:19 +0100

    add get all endpoint

--- client/Boundaries/Backend/Endpoints/GraphicalDataFormatEndpoints.cs
+++ client/Boundaries/Backend/Endpoints/GraphicalDataFormatEndpoints.cs
@@ -4,11 +4,19 @@ public static class GraphicalDataFormatEndpoints
 {
     public static void MapGraphicalDataFormatEndpoints(this IEndpointRouteBuilder endpoints)
     {
-        endpoints.MapGet("/graphical-data-format", async () =>
+        // Get graphical data format by P&ID id
+        endpoints.MapGet("/graphical-data-format/{id}", async (string id) =>
         {
+            // return example file
             var filePath = Path.Combine(AppContext.BaseDirectory, "Example", "graphical-data-format-example.json");
             var jsonContent = await File.ReadAllTextAsync(filePath);
             return Results.Json(jsonContent);
         }).WithTags("Graphical Data Format");
+
+        // Get all P&ID graphical data formats
+        endpoints.MapGet("/graphical-data-format/all", () =>
+        {
+            throw new NotImplementedException("TODO: Not implemented...");
+        });
     }
 }
