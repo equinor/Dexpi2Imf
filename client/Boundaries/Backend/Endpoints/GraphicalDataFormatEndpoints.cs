191641bacbea220dc5671c30e7453d49e666184a
Embla Øye <118742425+eoye@users.noreply.github.com>
2025-02-06 08:40:31 +0100

    Mock endpoint for graphical data format (#84)
    [Pull requests practices for the SSI
    team](https://github.com/equinor/ssi-infrastructure/blob/main/docs/pr_practices.md)
    
    ## Aim of the PR
    This PR fixes
    [AB#218925](https://dev.azure.com/EquinorASA/bb9bd8cb-74f7-4ffa-b0cb-60eff0a0be58/_workitems/edit/218925).
    
    The aim of this PR is to mock/stub endpoint for getting graphical data
    format for P&IDs.
    
    ## Implementation 
    This has been done by mocking an endpoint for getting the graphical data
    format for a P&ID by ID, which now return the example json file we have
    made. An endpoint for getting all P&IDs has also been stubbed.
    
    ## Type of change
    - [ ] Bug fix 
    - [x] New feature 
    - [ ] Breaking change 
    - [ ] This change requires a documentation update
    
    If the changes impact any dependent services then provide details.
    ## How Has This Been Tested?
    This has been tested by running the project and testing out the endpoint
    in swagger.
    
    ## Additional Changes
    I refactored the existing minimal API by splitting it up in it's own
    folders and files.

---
+++ client/Boundaries/Backend/Endpoints/GraphicalDataFormatEndpoints.cs
@@ -0,0 +1,24 @@
+﻿namespace Backend.Endpoints;
+
+public static class GraphicalDataFormatEndpoints
+{
+    public static void MapGraphicalDataFormatEndpoints(this IEndpointRouteBuilder endpoints)
+    {
+        // Get graphical data format by P&ID id
+        endpoints.MapGet("/graphical-data-format/{documentId}", async (string documentId) =>
+        {
+            var diagramId = Uri.UnescapeDataString(documentId);
+
+            // return example file
+            var filePath = Path.Combine(AppContext.BaseDirectory, "Example", "graphical-data-format-example.json");
+            var jsonContent = await File.ReadAllTextAsync(filePath);
+            return Results.Json(jsonContent);
+        }).WithTags("Graphical Data Format");
+
+        // Get all P&ID graphical data formats
+        endpoints.MapGet("/graphical-data-format/all", () =>
+        {
+            throw new NotImplementedException("TODO: Not implemented...");
+        });
+    }
+}
